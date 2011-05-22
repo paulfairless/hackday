73af49524e4e62ff1417c232aff7f243d5ecba09
Paul Fairless <paul.fairless@gmail.com>
2011-05-22 10:12:59 +0100

    added 320 grid and page

---
+++ hack.rb
@@ -0,0 +1,65 @@
+# hack.rb
+  require 'sinatra'
+  require 'rubygems'
+  require 'httparty'
+  require 'json'
+  
+  class SkyNews
+    include HTTParty
+  end
+  
+  helpers do
+    def cleanNewsML(newsml)
+      clean = {}
+      stories = []
+      
+      newsml["NewsML"]["NewsItem"].each{|key, value|
+        story = {}
+        newsitem = key["NewsComponent"]["NewsComponent"]["NewsComponent"]
+        story["headline"] = newsitem[0]["NewsLines"]["HeadLine"]
+        story["slug"] = newsitem[0]["NewsLines"]["SlugLine"]
+        story["body"] = newsitem[0]["ContentItem"]["DataContent"]
+        
+        imageIndex = newsitem[1]["ContentItem"].index{|item|item["Characteristics"]["Property"][0]["Value"]=='400'}
+        story["image"] =  newsitem[1]["ContentItem"].at(imageIndex)["Href"]
+        
+        # Do we have a video
+        if newsitem.size() > 2
+          videoWithPhoto = newsitem[2]["ContentItem"][0]
+          video = ""
+          if videoWithPhoto
+            video = newsitem[2]["ContentItem"][0]["Href"]
+            # story["image"] =  newsitem[2]["ContentItem"][1]["Href"]
+          else
+            video = newsitem[2]["ContentItem"]["Href"]
+          end
+          story["video"] = video
+        end
+        
+        stories.push story
+      }
+      # puts stories.to_s
+      clean["stories"] = stories
+      return clean
+    end
+  end
+  
+  get '/' do
+    'Hello world!'
+  end
+  
+  
+  get '/feed/:feed' do |f|
+    callback = params.delete('callback')
+    resp = HTTParty.get("http://news.sky.com/sky-news/newsml/#{f}/index.xml")
+    json = resp.parsed_response.to_json
+    json = cleanNewsML(resp.parsed_response).to_json
+    if callback
+      content_type :js
+      response = "#{callback}(#{json})" 
+    else
+      content_type :json
+      response = json
+    end
+    response
+  end
---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+(function($){
+	var main = $('div[role="main"]'),
+		nav = $("nav[role='navigation'] ul");
+ 	
+	var init = function(){
+		updateStories('uknews', main);
+		initNav();
+	}
+	
+	var updateStories = function(channel, container) {
+		$.getJSON('/feed/'+channel+'?callback=?', function(data){
+			console.log(data);
+			var items = data.stories
+			for (var newsItem in items){
+				$( "#mediaTemplate" ).tmpl( items[newsItem] ).appendTo( container );
+			}
+		});
+	}
+	
+	var initNav = function(){
+		$("li a", nav).click(function() {
+			$("li", nav).removeClass("selected");
+			$(this).parents().addClass("selected");
+			return false;
+		});	
+	}
+	
+	init();
+	
+})(jQuery);
