fc788502b50703c420709229b24871663304f65b
Paul Fairless <paul.fairless@gmail.com>
2011-05-23 10:29:32 +0100

    scrolling nav

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,13 @@
 (function($){
 	var main = $('div[role="main"]'),
-		nav = $("nav[role='navigation'] ul");
+		nav = $("nav[role='navigation'] ul"),
+		navPos = 0;
  	
 	var init = function(){
 		updateStories('uknews', main);
 		initNav();
+		initArrows();
+		initNavClick();
 	}
 	
 	var updateStories = function(channel, container) {
@@ -15,6 +18,29 @@
 			for (var newsItem in items){
 				$( "#mediaTemplate" ).tmpl( items[newsItem] ).appendTo( container );
 			}
+		});
+	}
+	
+	var initArrows = function(){
+		$('#left-arrow').click(function(ev){
+			navPos += 100;
+			
+			nav.css('left', navPos+'px');
+			ev.preventDefault();
+		});
+		
+		$('#right-arrow').click(function(ev){
+			navPos -= 100;
+			
+			nav.css('left', navPos+'px');
+			ev.preventDefault();
+		});
+	}
+	
+	var initNavClick = function(){
+		$("li a", nav).click(function(ev){
+			updateStories($(this).attr('data-channel'),main);
+			ev.preventDefault();
 		});
 	}
 	
